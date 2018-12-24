2fef093e5f52b7597db3dd686bf1ce1ca775f8c1
duwen <duwen.x@bytedance.com>
2018-12-24 18:04:56 +0800

    fix #96

--- example/test.dart
+++ example/test.dart
@@ -4,10 +4,19 @@ import 'package:dio/dio.dart';
 
 
 main() async {
+//  String s=Transformer.urlEncodeMap({
+//    'a': 1,
+//    'b': 2,
+//    "c":{
+//      "a":5,
+//      "b":6
+//    }
+//  });
+
   var dio = new Dio(Options(
     baseUrl: "https://www.baidu.com"
   ));
- Response response= await dio.get("http://www.w3school.com.cn/tags/html_ref_httpmethods.asp");
+ Response response= await dio.get("https://www.toutiao.com/stream/widget/local_weather/data/?city=%E4%B8%8A%E6%B5%B7");
  print(response);
 //  dio.interceptor.response.onError=(DioError error){
 //    print(error);
