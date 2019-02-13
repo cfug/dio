53821ae49c0a8973635dbbfb95c929b2091a29d6
duwen <duwen.x@bytedance.com>
2019-02-13 21:33:57 +0800

    update demo

--- example/proxy.dart
+++ example/proxy.dart
@@ -1,15 +1,18 @@
 import 'dart:io';
+import 'package:cookie_jar/cookie_jar.dart';
 import 'package:dio/dio.dart';
 
 main() async {
   var dio = new Dio();
   //dio.options.connectTimeout = 2000;
   // More about HttpClient proxy topic please refer to Dart SDK doc.
+  dio.interceptors.add(CookieManager(PersistCookieJar()));
   (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate = (HttpClient client) {
     client.findProxy = (uri) {
       //proxy all request to localhost:8888
       return "PROXY localhost:8888";
     };
+    client.badCertificateCallback=(X509Certificate cert, String host, int port)=>true;
   };
   var dir=new Directory("./cookies");
   await dir.create();
