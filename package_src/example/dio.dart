2d0a6d4d8be4a2a2ae52bb1833a8d94045f07093
duwen <duwen.x@bytedance.com>
2019-02-19 14:53:12 +0800

    refactor project structures

---
+++ package_src/example/dio.dart
@@ -0,0 +1,8 @@
+import 'package:dio/dio.dart';
+
+/// More examples see https://github.com/flutterchina/dio/tree/master/example
+main() async {
+  var dio = Dio();
+  Response response = await dio.get("https://google.com");
+  print(response.data);
+}
