00d484bbccdb110f4a09d6dadad13f935c6f3cc2
Thiago Santos <thiago.santos@moneyex.io>
2019-08-07 20:46:57 -0300

    Added safe query example

--- example/safe_query.dart
+++ example/safe_query.dart
@@ -1,8 +1,45 @@
 import 'package:dio/dio.dart';
 
-/// More examples see https://github.com/flutterchina/dio/tree/master/example
-main() async {
+void _callDefaultRequest() async {
   var dio = Dio();
-  Response response = await dio.get("https://google.com");
-  print(response.data);
+  dio.interceptors.add(LogInterceptor(request: false, responseHeader: false));
+  dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (RequestOptions options) {
+        assert(options.queryParameters.isEmpty);
+        return options;
+      }
+  ));
+  await dio.get("https://google.com");
+}
+
+void _safeQueryEnabledByDefault() async {
+  var dio = Dio();
+  dio.interceptors.add(LogInterceptor(request: false, responseHeader: false));
+  dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (RequestOptions options) {
+        assert(options.queryParameters.isEmpty);
+        return options;
+      }
+  ));
+  await dio.get("https://google.com", queryParameters: {'something': null});
+}
+
+void _safeQueryDisabled() async {
+  var dio = Dio();
+  dio.interceptors.add(LogInterceptor(request: false, responseHeader: false));
+  dio.interceptors.add(InterceptorsWrapper(
+      onRequest: (RequestOptions options) {
+        var queryParameters = options.queryParameters;
+        assert(queryParameters.isNotEmpty);
+        assert(queryParameters['something'] == null);
+        return options;
+      }
+  ));
+  await dio.get("https://google.com", queryParameters: {'something': null}, safeQuery: false);
+}
+
+main() async {
+  _callDefaultRequest();
+  _safeQueryEnabledByDefault();
+  _safeQueryDisabled();
 }
