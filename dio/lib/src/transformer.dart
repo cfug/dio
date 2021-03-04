6b0fc79cb00ef4e0baad058146b32194a156cefe
wendux <824783146@qq.com>
2021-03-04 21:40:48 +0800

    update flutter demo

--- dio/lib/src/transformer.dart
+++ dio/lib/src/transformer.dart
@@ -34,11 +34,11 @@ abstract class Transformer {
 
   /// Deep encode the [Map<String, dynamic>] to percent-encoding.
   /// It is mostly used with  the "application/x-www-form-urlencoded" content-type.
-  static String urlEncodeMap(Map map,[ListFormat collectionFormat = ListFormat.multi]) {
+  static String urlEncodeMap(Map map,[ListFormat listFormat = ListFormat.multi]) {
     return encodeMap(map, (key, value) {
       if (value == null) return key;
       return '$key=${Uri.encodeQueryComponent(value.toString())}';
-    },listFormat: collectionFormat);
+    },listFormat: listFormat);
   }
 }
 
---
+++ example/flutter_app/android/app/src/main/java/club/flutterchina/flutter_app/MainActivity.java
@@ -0,0 +1,6 @@
+package club.flutterchina.flutter_app;
+
+import io.flutter.embedding.android.FlutterActivity;
+
+public class MainActivity extends FlutterActivity {
+}
