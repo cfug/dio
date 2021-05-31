d69fe19b60a87cf76c9d8a5a7d36480901315bc7
Peter Leibiger <kuhnroyal@gmail.com>
2021-05-31 18:40:31 +0200

    Export ListParam and add toString/equals/hashCode
    * order imports in main file

--- dio/lib/dio.dart
+++ dio/lib/dio.dart
@@ -5,17 +5,18 @@
 /// A open source project authorized by [https://flutterchina.club](https://flutterchina.club).
 library dio;
 
+export 'src/adapter.dart';
+export 'src/cancel_token.dart';
 export 'src/dio.dart';
+export 'src/dio_error.dart';
 export 'src/dio_mixin.dart';
 export 'src/form_data.dart';
-export 'src/dio_error.dart';
-export 'src/transformer.dart';
-export 'src/interceptor.dart' hide InterceptorState, InterceptorResultType;
-export 'src/options.dart';
-export 'src/response.dart';
-export 'src/cancel_token.dart';
-export 'src/multipart_file.dart';
-export 'src/adapter.dart';
 export 'src/headers.dart';
+export 'src/interceptor.dart' hide InterceptorState, InterceptorResultType;
 export 'src/interceptors/log.dart';
+export 'src/multipart_file.dart';
+export 'src/options.dart';
+export 'src/parameter.dart';
 export 'src/redirect_record.dart';
+export 'src/response.dart';
+export 'src/transformer.dart';
--- dio/lib/src/parameter.dart
+++ dio/lib/src/parameter.dart
@@ -5,4 +5,20 @@ class ListParam<T> {
   List<T> value;
 
   ListParam(this.value, this.format);
+
+  @override
+  String toString() {
+    return 'ListParam{format: $format, value: $value}';
+  }
+
+  @override
+  bool operator ==(Object other) =>
+      identical(this, other) ||
+      other is ListParam &&
+          runtimeType == other.runtimeType &&
+          format == other.format &&
+          value == other.value;
+
+  @override
+  int get hashCode => format.hashCode ^ value.hashCode;
 }
