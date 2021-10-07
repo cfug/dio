0961384b4d75cb08a0d16ece0f4e7ed1cb867399
Lee kang-yong <yebgi83@gmail.com>
2021-10-07 17:57:45 +0900

    FIX : Add unit test about readtimeout

---
+++ dio/test/readtimeout_test.dart
@@ -0,0 +1,95 @@
+import 'dart:async';
+import 'dart:io';
+import 'dart:isolate';
+import 'package:pedantic/pedantic.dart';
+import 'package:dio/dio.dart';
+import 'package:test/test.dart';
+
+const SLEEP_DURATION_AFTER_CONNECTION_ESTABLISHED = 5000;
+
+HttpServer? _server;
+
+late Uri serverUrl;
+
+Future<int> getUnusedPort() async {
+  HttpServer? server;
+  try {
+    server = await HttpServer.bind('localhost', 0);
+    return server.port;
+  }  finally {
+    unawaited(server?.close());
+  }
+}
+
+void startServer(port) async {
+  _server = await HttpServer.bind('localhost', port);
+  _server?.listen((request) {
+      const content = 'success';
+      var response = request.response;
+
+      sleep(const Duration(milliseconds: SLEEP_DURATION_AFTER_CONNECTION_ESTABLISHED));
+
+      response
+        ..statusCode = 200
+        ..contentLength = content.length
+        ..write(content);
+
+      response.close();
+      return;
+    });
+}
+
+void stopServer() {
+  if (_server != null) {
+    _server!.close();
+    _server = null;
+  }
+}
+
+void main() {
+  setUp(() async {
+    var port = await getUnusedPort();
+    serverUrl = Uri.parse('http://localhost:$port');
+    unawaited(Isolate.spawn(startServer, port));
+  });
+
+  tearDown(stopServer);
+
+  test('#read_timeout - catch DioError when receiveTimeout < $SLEEP_DURATION_AFTER_CONNECTION_ESTABLISHED', () async {
+    var dio = Dio();
+
+    dio.options
+      ..baseUrl = serverUrl.toString()
+      ..receiveTimeout = SLEEP_DURATION_AFTER_CONNECTION_ESTABLISHED - 1000;
+
+    DioError error;
+
+    try {
+      await dio.get('/');
+      fail('did not throw');
+    } on DioError catch (e) {
+      error = e;
+    }
+
+    expect(error, isNotNull);
+    expect(error.type == DioErrorType.receiveTimeout, isTrue);
+  });
+
+  test('#read_timeout - no DioError when receiveTimeout > $SLEEP_DURATION_AFTER_CONNECTION_ESTABLISHED', () async {
+    var dio = Dio();
+
+    dio.options
+      ..baseUrl = serverUrl.toString()
+      ..receiveTimeout = SLEEP_DURATION_AFTER_CONNECTION_ESTABLISHED + 1000;
+
+    DioError? error;
+
+    try {
+      await dio.get('/');
+    } on DioError catch (e) {
+      error = e;
+    }
+
+    expect(error, isNull);
+  });
+}
