d01309bd07f06f7f6a3385918d0b4463601fca84
Peter Leibiger <kuhnroyal@gmail.com>
2021-11-24 18:06:43 +0100

    Add basic CI workflow

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 cd dio
-flutter test --coverage .
+dart test --coverage=coverage .
+pub run coverage:format_coverage --packages=.packages -i coverage -o coverage/lcov.info --lcov
 genhtml -o coverage coverage/lcov.info
 # Open in the default browser (mac):
 open coverage/index.html
-
