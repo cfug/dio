e0bb1c5e50b777d49b18684c929cc0ce15971d38
YFdyh000 <yfdyh000@gmail.com>
2022-02-13 20:19:48 +0800

    add badssl to exception_test.dart

--- dio/test/exception_test.dart
+++ dio/test/exception_test.dart
@@ -1,5 +1,7 @@
+import 'dart:io';
 import 'package:dio/dio.dart';
 import 'package:test/test.dart';
+import 'package:dio/adapter.dart';
 
 void main() {
   test('catch DioError', () async {
@@ -29,4 +31,32 @@ void main() {
     expect(error, isNotNull);
     expect(error is Exception, isTrue);
   });
+
+  test('catch sslerror: hostname mismatch', () async {
+    dynamic error;
+
+    try {
+      await Dio().get('https://wrong.host.badssl.com/');
+      fail('did not throw');
+    } on DioError catch (e) {
+      error = e;
+    }
+    expect(error, isNotNull);
+    expect(error is Exception, isTrue);
+  });
+
+  test('allow badssl', () async {
+    var dio = Dio();
+    (dio.httpClientAdapter as DefaultHttpClientAdapter).onHttpClientCreate =
+        (HttpClient client) {
+      client.badCertificateCallback =
+          (X509Certificate cert, String host, int port) => true;
+    };
+    var response = await dio.get('https://wrong.host.badssl.com/');
+    expect(response.statusCode, 200);
+    response = await dio.get('https://expired.badssl.com/');
+    expect(response.statusCode, 200);
+    response = await dio.get('https://self-signed.badssl.com/');
+    expect(response.statusCode, 200);
+  }, testOn: "!browser");
 }
