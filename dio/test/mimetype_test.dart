1d7e5947e45e0b94be1cd25e86d0beecf15c32b3
Lukas Kirner <lkirner@inovex.de>
2022-03-04 14:57:35 +0100

    Correct JSON MIME Type detection

---
+++ dio/test/mimetype_test.dart
@@ -0,0 +1,16 @@
+import 'package:dio/dio.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('JSON MimeType "application/json" ', () {
+    expect(Transformer.isJsonMimeType("application/json"), isTrue);
+  });
+
+  test('JSON MimeType "text/json" ', () {
+    expect(Transformer.isJsonMimeType("text/json"), isTrue);
+  });
+
+  test('JSON MimeType "application/vnd.example.com+json" ', () {
+    expect(Transformer.isJsonMimeType("text/json"), isTrue);
+  });
+}
