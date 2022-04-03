38349961603c91606c0cf2c171f338700c76bf92
ipcjs <gipcjs@gmail.com>
2022-04-04 05:09:11 +0800

    Split into lock and queue

--- example/lib/queued_interceptor_crsftoken.dart
+++ example/lib/queued_interceptor_crsftoken.dart
@@ -5,95 +5,18 @@ import 'dart:typed_data';
 
 import 'package:dio/dio.dart';
 
-const _useLock = false;
-
 void main() async {
   var dio = Dio();
+  dio.httpClientAdapter = _MockAdapter();
   //  dio instance to request token
-  var tokenDio = Dio();
   dio.options.baseUrl = 'http://www.dtworkroom.com/doris/1/2.0.0/';
-  tokenDio.options = dio.options;
-  dio.interceptors
-      .add((_useLock ? InterceptorsWrapper.new : QueuedInterceptorsWrapper.new)(
-    onRequest: (options, handler) {
-      print('send request：path:${options.path}，baseURL:${options.baseUrl}');
-      if (csrfToken == null) {
-        print('no token，request token firstly...');
-        if (_useLock) dio.lock();
-        tokenDio.get('/token').then((d) {
-          options.headers['csrfToken'] = csrfToken = d.data['data']['token'];
-          print('request token succeed, value: ' + d.data['data']['token']);
-          print(
-              'continue to perform request：path:${options.path}，baseURL:${options.path}');
-          handler.next(options);
-        }).catchError(
-          (error, stackTrace) {
-            handler.reject(error, true);
-          },
-          test: (e) => e is DioError,
-        ).whenComplete(() {
-          if (_useLock) dio.unlock();
-        });
-      } else {
-        options.headers['csrfToken'] = csrfToken;
-        return handler.next(options);
-      }
-    },
-    onError: (error, handler) {
-      //print(error);
-      // Assume 401 stands for token expired
-      if (error.response?.statusCode == 401) {
-        var options = error.response!.requestOptions;
-        // If the token has been updated, repeat directly.
-        if (csrfToken != options.headers['csrfToken']) {
-          options.headers['csrfToken'] = csrfToken;
-          //repeat
-          dio.fetch(options).then(
-            (r) {
-              handler.resolve(r);
-            },
-            onError: (e) {
-              handler.reject(e);
-            },
-          );
-          return;
-        }
-        if (_useLock) {
-          dio.lock();
-          dio.interceptors.responseLock.lock();
-          dio.interceptors.errorLock.lock();
-        }
-        tokenDio.get('/token').then((d) {
-          //update csrfToken
-          options.headers['csrfToken'] = csrfToken = d.data['data']['token'];
-        }).whenComplete(() {
-          if (_useLock) {
-            dio.unlock();
-            dio.interceptors.responseLock.unlock();
-            dio.interceptors.errorLock.unlock();
-          }
-        }).then((e) {
-          //repeat
-          dio.fetch(options).then(
-            (r) {
-              handler.resolve(r);
-            },
-            onError: (e) {
-              handler.reject(e);
-            },
-          );
-        });
-        return;
-      }
-      return handler.next(error);
-    },
-  ));
+  dio.interceptors.add(false //
+      ? LockTokenInterceptor(dio)
+      : QueueTokenInterceptor(dio));
 
   FutureOr<void> _onResult(d) {
     print('request ok!: $d');
   }
-
-  dio.httpClientAdapter = tokenDio.httpClientAdapter = _MockAdapter();
 
   await Future.wait([
     dio.get('test?tag=1').then(_onResult),
@@ -103,6 +26,163 @@ void main() async {
 }
 
 String? csrfToken;
+
+class LockTokenInterceptor extends Interceptor {
+  LockTokenInterceptor(this.dio) {
+    tokenDio = Dio()
+      ..options = dio.options
+      ..httpClientAdapter = dio.httpClientAdapter;
+  }
+  final Dio dio;
+  late final Dio tokenDio;
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    print('send request：path:${options.path}，baseURL:${options.baseUrl}');
+    if (csrfToken == null) {
+      print('no token，request token firstly...');
+      dio.lock();
+      tokenDio.get('/token').then((d) {
+        options.headers['csrfToken'] = csrfToken = d.data['data']['token'];
+        print('request token succeed, value: ' + d.data['data']['token']);
+        print(
+            'continue to perform request：path:${options.path}，baseURL:${options.path}');
+        handler.next(options);
+      }).catchError(
+        (error, stackTrace) {
+          handler.reject(error, true);
+        },
+        test: (e) => e is DioError,
+      ).whenComplete(() {
+        dio.unlock();
+      });
+    } else {
+      options.headers['csrfToken'] = csrfToken;
+      return handler.next(options);
+    }
+  }
+
+  @override
+  void onError(DioError error, ErrorInterceptorHandler handler) {
+    //print(error);
+    // Assume 401 stands for token expired
+    if (error.response?.statusCode == 401) {
+      var options = error.response!.requestOptions;
+      // If the token has been updated, repeat directly.
+      if (csrfToken != options.headers['csrfToken']) {
+        options.headers['csrfToken'] = csrfToken;
+        //repeat
+        dio.fetch(options).then(
+          (r) {
+            handler.resolve(r);
+          },
+          onError: (e) {
+            handler.reject(e);
+          },
+        );
+        return;
+      }
+
+      dio.lock();
+      dio.interceptors.responseLock.lock();
+      dio.interceptors.errorLock.lock();
+
+      tokenDio.get('/token').then((d) {
+        //update csrfToken
+        options.headers['csrfToken'] = csrfToken = d.data['data']['token'];
+      }).whenComplete(() {
+        dio.unlock();
+        dio.interceptors.responseLock.unlock();
+        dio.interceptors.errorLock.unlock();
+      }).then((e) {
+        //repeat
+        dio.fetch(options).then(
+          (r) {
+            handler.resolve(r);
+          },
+          onError: (e) {
+            handler.reject(e);
+          },
+        );
+      });
+      return;
+    }
+    return handler.next(error);
+  }
+}
+
+class QueueTokenInterceptor extends QueuedInterceptor {
+  QueueTokenInterceptor(this.dio) {
+    tokenDio = Dio()
+      ..options = dio.options
+      ..httpClientAdapter = dio.httpClientAdapter;
+  }
+  final Dio dio;
+  late final Dio tokenDio;
+
+  @override
+  void onRequest(RequestOptions options, RequestInterceptorHandler handler) {
+    print('send request：path:${options.path}，baseURL:${options.baseUrl}');
+    if (csrfToken == null) {
+      print('no token，request token firstly...');
+      tokenDio.get('/token').then((d) {
+        options.headers['csrfToken'] = csrfToken = d.data['data']['token'];
+        print('request token succeed, value: ' + d.data['data']['token']);
+        print(
+            'continue to perform request：path:${options.path}，baseURL:${options.path}');
+        handler.next(options);
+      }).catchError(
+        (error, stackTrace) {
+          handler.reject(error, true);
+        },
+        test: (e) => e is DioError,
+      );
+    } else {
+      options.headers['csrfToken'] = csrfToken;
+      return handler.next(options);
+    }
+  }
+
+  @override
+  void onError(DioError error, ErrorInterceptorHandler handler) {
+    //print(error);
+    // Assume 401 stands for token expired
+    if (error.response?.statusCode == 401) {
+      var options = error.response!.requestOptions;
+      // If the token has been updated, repeat directly.
+      if (csrfToken != options.headers['csrfToken']) {
+        options.headers['csrfToken'] = csrfToken;
+        //repeat
+        dio.fetch(options).then(
+          (r) {
+            handler.resolve(r);
+          },
+          onError: (e) {
+            handler.reject(e);
+          },
+        );
+        return;
+      }
+
+      tokenDio.get('/token').then((d) {
+        //update csrfToken
+        options.headers['csrfToken'] = csrfToken = d.data['data']['token'];
+      }).then((e) {
+        //repeat
+        dio.fetch(options).then(
+          (r) {
+            handler.resolve(r);
+          },
+          onError: (e) {
+            handler.reject(e);
+          },
+        );
+      });
+      return;
+    }
+    return handler.next(error);
+  }
+}
 
 final _kRandom = Random(0);
 
