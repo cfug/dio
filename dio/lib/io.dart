a8a9fd88f1f86eb1b93a36d673588ab502487dab
Alex Li <github@alexv525.com>
2022-11-15 16:14:46 +0800

    Show `IOHttpClientAdapter` (#22)
    Signed-off-by: Alex Li <github@alexv525.com>

--- dio/lib/io.dart
+++ dio/lib/io.dart
@@ -1,2 +1,3 @@
 export 'src/entry/dio_for_native.dart' show DioForNative;
-export 'src/adapters/io_adapter.dart' show DefaultHttpClientAdapter;
+export 'src/adapters/io_adapter.dart'
+    show DefaultHttpClientAdapter, IOHttpClientAdapter;
