75e03ed68d996a3aa4ee68554c699f005698bd9c
孙娇 <P.ozzy@outlook.com>
2023-02-19 00:50:56 +0800

    Add test case for cors with credentials (#1663)

---
+++ dio/test/browser_test.dart
@@ -0,0 +1,27 @@
+@TestOn('chrome')
+
+import 'package:dio/browser.dart';
+import 'package:dio/dio.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('with credentials', () async {
+    // run ../example/server/server.dart before doing the following test.
+    final browserAdapter = BrowserHttpClientAdapter();
+    browserAdapter.withCredentials = true;
+    final options = BaseOptions(
+      baseUrl: 'http://127.0.0.1:2384',
+    );
+    final dio = DioForBrowser(options);
+    dio.httpClientAdapter = browserAdapter;
+
+    await dio
+        .get('/cors/getCookie', queryParameters: {'value': 'Dio'});
+
+    final response = await dio
+        .get('/cors/checkCookie', queryParameters: {'value': 'Dio'});
+
+    expect(response.statusCode, 200);
+  });
+
+}
