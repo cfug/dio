541eeded8278e311b40d7d96f9127aec2923756a
Sun Jiao <P.ozzy@outlook.com>
2023-02-19 14:04:29 +0800

    Update dio/test/browser_test.dart
    Co-authored-by: Alex Li <github@alexv525.com>
    Signed-off-by: Sun Jiao <P.ozzy@outlook.com>

--- dio/test/browser_test.dart
+++ dio/test/browser_test.dart
@@ -15,11 +15,16 @@ void main() {
     final dio = DioForBrowser(options);
     dio.httpClientAdapter = browserAdapter;
 
-    await dio
-        .get('/cors/getCookie', queryParameters: {'value': 'Dio'});
-
-    final response = await dio
-        .get('/cors/checkCookie', queryParameters: {'value': 'Dio'});
+    // Request to get the cookies from the server.
+    await dio.get(
+      '/cors/getCookie',
+      queryParameters: {'value': 'Dio'},
+    );
+    // Request again to verify if the cookies are obtained and have been pass-through.
+    final response = await dio.get(
+      '/cors/checkCookie',
+      queryParameters: {'value': 'Dio'},
+    );
 
     expect(response.statusCode, 200);
   });
