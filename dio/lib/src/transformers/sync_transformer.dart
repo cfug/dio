60a6ea241108f50b65ca8f74bc2045fd120142b0
Alex Li <github@alexv525.com>
2023-05-09 22:30:11 +0800

    📝 transformers ++

--- dio/lib/src/transformers/sync_transformer.dart
+++ dio/lib/src/transformers/sync_transformer.dart
@@ -16,11 +16,9 @@ typedef JsonDecodeCallback = FutureOr<dynamic> Function(String);
 /// The callback definition for encoding a JSON object.
 typedef JsonEncodeCallback = FutureOr<String> Function(Object);
 
-/// The default [Transformer] for [Dio].
-///
 /// If you want to custom the transformation of request/response data,
 /// you can provide a [Transformer] by your self, and replace
-/// the [DefaultTransformer] by setting the [dio.transformer].
+/// the transformer by setting the [Dio.transformer].
 class SyncTransformer extends Transformer {
   SyncTransformer({
     this.jsonDecodeCallback = jsonDecode,
