5f6b220a3b40c8fbe0cb2cf8f05f2d0cdb997291
Sahil Kumar <xdsahil@gmail.com>
2023-06-06 13:47:23 +0530

    fix(dio): make DioError deprecation non-breaking.
    Signed-off-by: xsahil03x <xdsahil@gmail.com>

--- dio/lib/dio.dart
+++ dio/lib/dio.dart
@@ -6,6 +6,7 @@ library dio;
 export 'src/adapter.dart';
 export 'src/cancel_token.dart';
 export 'src/dio.dart';
+export 'src/dio_error.dart';
 export 'src/dio_exception.dart';
 export 'src/dio_mixin.dart' hide InterceptorState, InterceptorResultType;
 export 'src/form_data.dart';
