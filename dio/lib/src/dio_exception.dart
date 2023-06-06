a6fc168412e1545052d51cbebb170f3dd8dfa0e6
Cristian Zazo <cristian.zazo@gmail.com>
2023-06-06 10:58:45 +0200

    Fix deprecation warning
    Signed-off-by: Cristian Zazo <cristian.zazo@gmail.com>

--- dio/lib/src/dio_exception.dart
+++ dio/lib/src/dio_exception.dart
@@ -2,7 +2,7 @@ import 'options.dart';
 import 'response.dart';
 
 /// Deprecated in favor of [DioExceptionType] and will be removed in future major versions.
-@Deprecated('Use DioException instead. This will be removed in 6.0.0')
+@Deprecated('Use DioExceptionType instead. This will be removed in 6.0.0')
 typedef DioErrorType = DioExceptionType;
 
 /// [DioError] describes the exception info when a request failed.
