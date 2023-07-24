578ec7863cb7feb012525feaa5b2b43b2d83ba18
gabrielaraujoz <garaujo.eng@gmail.com>
2023-07-24 11:39:59 -0300

    add convenience method for cloning formdata and regression test for File error

--- dio/test/formdata_test.dart
+++ dio/test/formdata_test.dart
@@ -94,10 +94,64 @@ void main() async {
       testOn: 'vm',
     );
 
+    test(
+      'complex cloning FormData object',
+      () async {
+        final fm = FormData.fromMap({
+          'name': 'wendux',
+          'age': 25,
+          'path': '/图片空间/地址',
+          'file': MultipartFile.fromString(
+            'hello world.',
+            headers: {
+              'test': <String>['a']
+            },
+          ),
+          'files': [
+            await MultipartFile.fromFile(
+              'test/mock/_testfile',
+              filename: '1.txt',
+              headers: {
+                'test': <String>['b']
+              },
+            ),
+            MultipartFile.fromFileSync(
+              'test/mock/_testfile',
+              filename: '2.txt',
+              headers: {
+                'test': <String>['c']
+              },
+            ),
+          ]
+        });
+        final fmStr = await fm.readAsBytes();
+        final f = File('test/mock/_formdata');
+        String content = f.readAsStringSync();
+        content = content.replaceAll('--dio-boundary-3788753558', fm.boundary);
+        String actual = utf8.decode(fmStr, allowMalformed: true);
+
+        actual = actual.replaceAll('\r\n', '\n');
+        content = content.replaceAll('\r\n', '\n');
+
+        expect(actual, content);
+        expect(fm.readAsBytes(), throwsA(const TypeMatcher<StateError>()));
+
+        final fm1 = fm.clone();
+        expect(fm1.isFinalized, false);
+        final fm1Str = await fm1.readAsBytes();
+        expect(fmStr.length, fm1Str.length);
+        expect(fm1.isFinalized, true);
+        expect(fm1 != fm, true);
+        expect(fm1.files[0].value.filename, fm.files[0].value.filename);
+        expect(fm1.fields, fm.fields);
+      },
+      testOn: 'vm',
+    );
+
     // Cloned multipart files should be able to be read again and be the same
     // as the original ones.
     test(
-      'complex with cloning',
+      'complex cloning MultipartFile',
       () async {
         final multipartFile1 = MultipartFile.fromString(
           'hello world.',
