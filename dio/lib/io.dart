6ac878937dee28b7ca62a2a502cb6f95d4b98308
Alex Li <github@alexv525.com>
2023-07-30 10:08:31 +0800

    🎨 io ++

--- dio/lib/io.dart
+++ dio/lib/io.dart
@@ -1,4 +1,2 @@
-// ignore_for_file: deprecated_member_use, deprecated_member_use_from_same_package
-export 'src/adapters/io_adapter.dart'
-    show DefaultHttpClientAdapter, IOHttpClientAdapter;
+export 'src/adapters/io_adapter.dart' hide createAdapter;
 export 'src/dio/dio_for_native.dart' show DioForNative;
