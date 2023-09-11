8e13c7b667ca5e2173238047dd8d74e59bffa7fc
Peter Leibiger <kuhnroyal@gmail.com>
2023-09-11 14:53:15 +0200

    Improve melos configuration
    * rename workspace project to avoid conflicts in IDEA (2x dio project)
    * add examples to melos
    * add .gitignore where missing

--- example_flutter_app/test/widget_test.dart
+++ example_flutter_app/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_app/main.dart';
+import 'package:dio_flutter_example/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
