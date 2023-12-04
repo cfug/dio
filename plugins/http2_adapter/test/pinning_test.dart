48b26b886c078fa86113353779df57ba8e2522a7
Peter Leibiger <kuhnroyal@gmail.com>
2023-12-04 16:50:15 +0100

    Skip tests requiring specific setup by default
    * use `-P all` in CI to run all tests

--- plugins/http2_adapter/test/pinning_test.dart
+++ plugins/http2_adapter/test/pinning_test.dart
@@ -7,15 +7,15 @@ import 'package:dio_http2_adapter/dio_http2_adapter.dart';
 import 'package:test/test.dart';
 
 void main() {
-  // NOTE: Run scripts/prepare_pinning_certs.sh
-  // to download the current certs to the file below.
-  //
-  // OpenSSL output like: SHA256 Fingerprint=EE:5C:E1:DF:A7:A4...
-  // All badssl.com hosts have the same cert, they just have TLS
-  // setting or other differences (like host name) that make them bad.
-  final lines = File('test/_pinning_http2.txt').readAsLinesSync();
-  final fingerprint =
-      lines.first.split('=').last.toLowerCase().replaceAll(':', '');
+  /// NOTE: Run scripts/prepare_pinning_certs.sh
+  /// to download the current certs to the file below.
+  String fingerprint() {
+    // OpenSSL output like: SHA256 Fingerprint=EE:5C:E1:DF:A7:A4...
+    // All badssl.com hosts have the same cert, they just have TLS
+    // setting or other differences (like host name) that make them bad.
+    final lines = File('test/_pinning.txt').readAsLinesSync();
+    return lines.first.split('=').last.toLowerCase().replaceAll(':', '');
+  }
 
   group('SSL pinning', () {
     final Dio dio = Dio()
@@ -112,51 +112,55 @@ void main() {
       expect(res.data.toString(), contains(expectedHostString));
     });
 
-    test('untrusted certificate tested and allowed', () async {
-      bool badCert = false;
-      bool approved = false;
-      String? badCertSubject;
-      String? approverSubject;
-      String? badCertSha256;
-      String? approverSha256;
+    test(
+      'untrusted certificate tested and allowed #2',
+      () async {
+        bool badCert = false;
+        bool approved = false;
+        String? badCertSubject;
+        String? approverSubject;
+        String? badCertSha256;
+        String? approverSha256;
 
-      dio.httpClientAdapter = Http2Adapter(
-        ConnectionManager(
-          idleTimeout: Duration(seconds: 10),
-          onClientCreate: (url, config) {
-            config.context = SecurityContext(withTrustedRoots: false);
-            config.onBadCertificate = (certificate) {
-              badCert = true;
-              badCertSubject = certificate.subject.toString();
-              badCertSha256 = sha256.convert(certificate.der).toString();
-              return true;
-            };
-            config.validateCertificate = (certificate, host, port) {
-              if (certificate == null) fail('must include a certificate');
-              approved = true;
-              approverSubject = certificate.subject.toString();
-              approverSha256 = sha256.convert(certificate.der).toString();
-              return true;
-            };
-          },
-        ),
-      );
+        dio.httpClientAdapter = Http2Adapter(
+          ConnectionManager(
+            idleTimeout: Duration(seconds: 10),
+            onClientCreate: (url, config) {
+              config.context = SecurityContext(withTrustedRoots: false);
+              config.onBadCertificate = (certificate) {
+                badCert = true;
+                badCertSubject = certificate.subject.toString();
+                badCertSha256 = sha256.convert(certificate.der).toString();
+                return true;
+              };
+              config.validateCertificate = (certificate, host, port) {
+                if (certificate == null) fail('must include a certificate');
+                approved = true;
+                approverSubject = certificate.subject.toString();
+                approverSha256 = sha256.convert(certificate.der).toString();
+                return true;
+              };
+            },
+          ),
+        );
 
-      final res = await dio.get('get');
-      expect(badCert, true);
-      expect(approved, true);
-      expect(badCertSubject, isNotNull);
-      expect(badCertSubject, isNot(contains(expectedHostString)));
-      expect(badCertSha256, isNot(fingerprint));
-      expect(approverSubject, isNotNull);
-      expect(approverSubject, contains(expectedHostString));
-      expect(approverSha256, fingerprint);
-      expect(approverSubject, isNot(badCertSubject));
-      expect(approverSha256, isNot(badCertSha256));
-      expect(res, isNotNull);
-      expect(res.data, isNotNull);
-      expect(res.data.toString(), contains(expectedHostString));
-    });
+        final res = await dio.get('get');
+        expect(badCert, true);
+        expect(approved, true);
+        expect(badCertSubject, isNotNull);
+        expect(badCertSubject, isNot(contains(expectedHostString)));
+        expect(badCertSha256, isNot(fingerprint));
+        expect(approverSubject, isNotNull);
+        expect(approverSubject, contains(expectedHostString));
+        expect(approverSha256, fingerprint);
+        expect(approverSubject, isNot(badCertSubject));
+        expect(approverSha256, isNot(badCertSha256));
+        expect(res, isNotNull);
+        expect(res.data, isNotNull);
+        expect(res.data.toString(), contains(expectedHostString));
+      },
+      tags: ['tls'],
+    );
 
     test('2 requests == 1 approval', () async {
       int approvalCount = 0;
