6adfdaeb3b916ca361a41abbd2502e80a9de0f2b
Alex Li <github@alexv525.com>
2024-06-04 12:57:03 +0800

    ⏪️ Reset

--- plugins/web_adapter/lib/src/js_interop/multipart_file.dart
+++ plugins/web_adapter/lib/src/js_interop/multipart_file.dart
@@ -1,23 +1,23 @@
-// export '../html/multipart_file.dart';
+export '../html/multipart_file.dart';
 
-import 'package:http_parser/http_parser.dart';
-
-final _err = UnsupportedError(
-  'MultipartFile is only supported where dart:io is available.',
-);
-
-Never multipartFileFromPath(
-  String filePath, {
-  String? filename,
-  MediaType? contentType,
-  final Map<String, List<String>>? headers,
-}) =>
-    throw _err;
-
-Never multipartFileFromPathSync(
-  String filePath, {
-  String? filename,
-  MediaType? contentType,
-  final Map<String, List<String>>? headers,
-}) =>
-    throw _err;
+// import 'package:http_parser/http_parser.dart';
+//
+// final _err = UnsupportedError(
+//   'MultipartFile is only supported where dart:io is available.',
+// );
+//
+// Never multipartFileFromPath(
+//   String filePath, {
+//   String? filename,
+//   MediaType? contentType,
+//   final Map<String, List<String>>? headers,
+// }) =>
+//     throw _err;
+//
+// Never multipartFileFromPathSync(
+//   String filePath, {
+//   String? filename,
+//   MediaType? contentType,
+//   final Map<String, List<String>>? headers,
+// }) =>
+//     throw _err;
