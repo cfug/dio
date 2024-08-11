5c61aad134bc4862293382db7e61e10f573c6427
Alex Li <github@alexv525.com>
2024-08-11 16:57:50 +0800

    🔥 Remove `html`

--- plugins/web_adapter/lib/dio_web_adapter.dart
+++ plugins/web_adapter/lib/dio_web_adapter.dart
@@ -1,24 +1,7 @@
 library dio_web_adapter;
 
-// We separate implementations as `html` and `js_interop`
-// only for convenient diffing.
-
-// export 'src/html/adapter.dart';
-
-export 'src/js_interop/adapter.dart';
-
-// export 'src/html/compute.dart';
-
-export 'src/js_interop/compute.dart';
-
-// export 'src/html/dio_impl.dart';
-
-export 'src/js_interop/dio_impl.dart';
-
-// export 'src/html/multipart_file.dart';
-
-export 'src/js_interop/multipart_file.dart';
-
-// export 'src/html/progress_stream.dart';
-
-export 'src/js_interop/progress_stream.dart';
+export 'src/adapter.dart';
+export 'src/compute.dart';
+export 'src/dio_impl.dart';
+export 'src/multipart_file.dart';
+export 'src/progress_stream.dart';
