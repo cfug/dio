27bb332c8ca2c2b9062fcd76c793e214d4377b5e
shehab mohame <shehabman@gmail.com>
2024-12-16 17:58:52 +0200

    Apply suggestions from code review
    Co-authored-by: Alex Li <github@alexv525.com>
    Signed-off-by: shehab mohame <shehabman@gmail.com>

--- dio/lib/src/file_access_mode.dart
+++ dio/lib/src/file_access_mode.dart
@@ -1,13 +1,14 @@
-/// {@template dio.FileAccessMode}
-///  The file access mode when downloading the file.
-/// - [FileAccessMode.write]: Mode for opening a file for reading and writing.
-///    The file is overwritten if it already exists. The file is created if it
-///    does not already exist
-/// - [FileAccessMode.append]: Mode for opening a file for reading and writing
-///    to the end of it. The file is created if it does not already exist.
+/// {@template dio.options.FileAccessMode}
+/// The file access mode when downloading a file, corresponds to a subset of 
+/// dart:io::[FileMode].
 /// {@endtemplate}
 enum FileAccessMode {
+  /// Mode for opening a file for reading and writing. The file is overwritten
+  /// if it already exists. The file is created if it does not already exist.
   write,
+
+  /// Mode for opening a file for reading and writing to the end of it.
+  /// The file is created if it does not already exist.
   append,
 }
 
