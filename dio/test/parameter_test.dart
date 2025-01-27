82e2f03eca0c5efb729f2b04cd46164c96fe64cf
suojae <suojae.developer@gmail.com>
2025-01-28 01:21:47 +0900

    test: refactor ListParam tests to use direct equality comparison and make group

--- dio/test/parameter_test.dart
+++ dio/test/parameter_test.dart
@@ -2,31 +2,23 @@ import 'package:dio/dio.dart';
 import 'package:test/test.dart';
 
 void main() {
-  test('ListParam equality for Map key', () {
-    final param1 = const ListParam(['item1', 'item2'], ListFormat.csv);
-    final param2 = const ListParam(['item1', 'item2'], ListFormat.csv);
-    final param3 = const ListParam(['item3', 'item4'], ListFormat.csv);
-    final param4 = const ListParam(['item2', 'item1'], ListFormat.csv);
+  group('ListParam', () {
+    test('param1 and param2 should be considered equal', () {
+      final param1 = const ListParam(['item1', 'item2'], ListFormat.csv);
+      final param2 = const ListParam(['item1', 'item2'], ListFormat.csv);
+      expect(param1 == param2, isTrue);
+    });
 
-    final cache = {param1: 'Cached Response'};
+    test('param1 and param3 should not be considered equal', () {
+      final param1 = const ListParam(['item1', 'item2'], ListFormat.csv);
+      final param3 = const ListParam(['item3', 'item4'], ListFormat.csv);
+      expect(param1 == param3, isFalse);
+    });
 
-    expect(
-      cache.containsKey(param2),
-      true,
-      reason: 'param1 and param2 should be considered equal',
-    );
-
-    expect(
-      cache.containsKey(param3),
-      false,
-      reason: 'param1 and param3 should not be considered equal',
-    );
-
-    expect(
-      cache.containsKey(param4),
-      false,
-      reason:
-          'param1 and param4 should not be considered equal as order matters',
-    );
+    test('Order matters: param1 and param4 should not be equal', () {
+      final param1 = const ListParam(['item1', 'item2'], ListFormat.csv);
+      final param4 = const ListParam(['item2', 'item1'], ListFormat.csv);
+      expect(param1 == param4, isFalse);
+    });
   });
 }
