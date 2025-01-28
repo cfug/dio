3324809155ebd33d316bd10b8f2a342db2ab8d16
Alex Li <github@alexv525.com>
2025-01-29 01:14:50 +0800

    ✅ Add tests

--- dio/test/dio_mixin_test.dart
+++ dio/test/dio_mixin_test.dart
@@ -1,3 +1,5 @@
+import 'dart:typed_data';
+
 import 'package:dio/dio.dart';
 import 'package:test/test.dart';
 
@@ -28,8 +30,42 @@ void main() {
       throwsA(const TypeMatcher<UnimplementedError>()),
     );
   });
+
+  test('cloned', () {
+    final dio = Dio();
+    final cloned = dio.clone();
+    expect(dio == cloned, false);
+    expect(dio.options, equals(cloned.options));
+    expect(dio.interceptors, equals(cloned.interceptors));
+    expect(dio.httpClientAdapter, equals(cloned.httpClientAdapter));
+    expect(dio.transformer, equals(cloned.transformer));
+    final clonedWithFields = dio.clone(
+      options: BaseOptions(baseUrl: 'http://localhost'),
+      interceptors: Interceptors()..add(InterceptorsWrapper()),
+      httpClientAdapter: _TestAdapter(),
+      transformer: SyncTransformer(),
+    );
+    expect(clonedWithFields.options.baseUrl, equals('http://localhost'));
+    expect(clonedWithFields.interceptors.length, equals(2));
+    expect(clonedWithFields.httpClientAdapter, isA<_TestAdapter>());
+    expect(clonedWithFields.transformer, isA<SyncTransformer>());
+  });
 }
 
 class _TestDioMixin with DioMixin implements Dio {}
 
 class _TestDioMixinExtends extends DioMixin implements Dio {}
+
+class _TestAdapter implements HttpClientAdapter {
+  @override
+  Future<ResponseBody> fetch(
+    RequestOptions options,
+    Stream<Uint8List>? requestStream,
+    Future<void>? cancelFuture,
+  ) {
+    throw UnimplementedError();
+  }
+
+  @override
+  void close({bool force = false}) {}
+}
