48913ce477077e2d36899df56650fb46d18ff9ee
Alex Li <github@alexv525.com>
2025-02-21 11:33:24 +0800

    ♻️ Move all source Dart files to `*_impl.dart` to avoid naming collision

--- plugins/web_adapter/lib/dio_web_adapter.dart
+++ plugins/web_adapter/lib/dio_web_adapter.dart
@@ -1,7 +1,7 @@
 library dio_web_adapter;
 
-export 'src/adapter.dart';
-export 'src/compute.dart';
+export 'src/adapter_impl.dart';
+export 'src/compute_impl.dart';
 export 'src/dio_impl.dart';
-export 'src/multipart_file.dart';
-export 'src/progress_stream.dart';
+export 'src/multipart_file_impl.dart';
+export 'src/progress_stream_impl.dart';
