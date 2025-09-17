42d9d661866f3467fc8b3a5aa80a99950c7df55d
LukasMirbt <lukas.mirbt@hotmail.com>
2025-09-17 19:48:35 +0200

    Fall back on IOClient when CronetEngine initialization fails

--- plugins/native_dio_adapter/lib/src/cronet_adapter.dart
+++ plugins/native_dio_adapter/lib/src/cronet_adapter.dart
@@ -1,7 +1,10 @@
+import 'dart:developer';
 import 'dart:typed_data' show Uint8List;
 
 import 'package:cronet_http/cronet_http.dart';
 import 'package:dio/dio.dart';
+import 'package:http/http.dart';
+import 'package:http/io_client.dart';
 import 'conversion_layer_adapter.dart';
 
 /// A [HttpClientAdapter] for Dio which delegates HTTP requests
@@ -11,16 +14,32 @@ class CronetAdapter implements HttpClientAdapter {
   CronetAdapter(
     CronetEngine? engine, {
     bool closeEngine = true,
-  }) : _conversionLayer = ConversionLayerAdapter(
-          engine == null
-              ? CronetClient.defaultCronetEngine()
-              : CronetClient.fromCronetEngine(engine, closeEngine: closeEngine),
-        );
+  })  : _engine = engine,
+        _closeEngine = closeEngine;
 
-  final ConversionLayerAdapter _conversionLayer;
+  final CronetEngine? _engine;
+  final bool _closeEngine;
 
-  /// The underlying conversion layer adapter.
-  ConversionLayerAdapter get adapter => _conversionLayer;
+  late final ConversionLayerAdapter _conversionLayer = () {
+    Client client;
+
+    try {
+      client = CronetClient.fromCronetEngine(
+        _engine ?? CronetEngine.build(),
+        closeEngine: _closeEngine,
+      );
+    } catch (error, stackTrace) {
+      log(
+        'Failed to create CronetClient, falling back to IOClient',
+        error: error,
+        stackTrace: stackTrace,
+      );
+
+      client = IOClient();
+    }
+
+    return ConversionLayerAdapter(client);
+  }();
 
   @override
   void close({bool force = false}) => _conversionLayer.close(force: force);
